eb710c952e7771d79e97a4c494d4f8eeb8bbda25
dani <daniel.h-962@freenet.de>
2021-12-23 18:11:11 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FriendsComponent} from './components/friends/friends.component';
+import {PostsComponent} from './components/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'friends', component: FriendsComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: '**', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
+import {PostService} from './services/post.service';
+import {HttpClientModule} from '@angular/common/http';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import {FormsModule} from '@angular/forms';
+import { CommentsComponent } from './components/comments/comments.component';
+import {CommentService} from './services/comment.service';
+import { FriendsComponent } from './components/friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent,
+    PostFormComponent,
+    CommentsComponent,
+    FriendsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PostService, CommentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
